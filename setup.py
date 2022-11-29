b8a17e9a7f0f4db7d4e6c56fcc37d984300512f4
Toby Drane <tdrane@no10.gov.uk>
2022-11-29 14:33:35 +0000

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="rapid",
-    version="0.0.6",
+    version="0.0.7",
     description="A python sdk for the rAPId API",
     url="https://github.com/no10ds/rapid-sdk",
     author="Lewis Card",
